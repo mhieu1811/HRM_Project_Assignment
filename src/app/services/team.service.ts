b00bca891689564a46aebfcb6118d5a6f5f2e8a1
Hieu Lai Minh <hieu.laiminh@nashtechglobal.com>
2023-02-16 10:36:07 +0700

    add team service

---
+++ src/app/services/team.service.ts
@@ -0,0 +1,124 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+import { IMessage } from '../interfaces/IMessage.interface';
+import { ITeam } from '../interfaces/ITeam.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TeamService {
+  constructor(private http: HttpClient, private cookieService: CookieService) {}
+
+  getListTeam(): Observable<Array<ITeam>> {
+    const header = {
+      headers: new HttpHeaders().set(
+        'Authorization',
+        `Bearer ${this.cookieService.get('userToken')}`
+      ),
+    };
+
+    return this.http.get<Array<ITeam>>(environment.api_url + '/teams/', header);
+  }
+
+  getTeamDetails(teamId: string): Observable<ITeam> {
+    const header = {
+      headers: new HttpHeaders().set(
+        'Authorization',
+        `Bearer ${this.cookieService.get('userToken')}`
+      ),
+    };
+
+    return this.http.get<ITeam>(
+      environment.api_url + '/teams/' + teamId,
+      header
+    );
+  }
+
+  addTeam(team: ITeam): Observable<ITeam> {
+    const header = {
+      headers: new HttpHeaders().set(
+        'Authorization',
+        `Bearer ${this.cookieService.get('userToken')}`
+      ),
+    };
+
+    return this.http.post<ITeam>(environment.api_url + '/teams/', team, header);
+  }
+
+  updateTeam(team: ITeam, teamID: string): Observable<ITeam> {
+    const header = {
+      headers: new HttpHeaders().set(
+        'Authorization',
+        `Bearer ${this.cookieService.get('userToken')}`
+      ),
+    };
+
+    return this.http.put<ITeam>(
+      environment.api_url + '/teams/' + teamID,
+      team,
+      header
+    );
+  }
+
+  deleteTeam(teamID: string): Observable<ITeam> {
+    const header = {
+      headers: new HttpHeaders().set(
+        'Authorization',
+        `Bearer ${this.cookieService.get('userToken')}`
+      ),
+    };
+
+    return this.http.delete<ITeam>(
+      environment.api_url + '/teams/' + teamID,
+      header
+    );
+  }
+
+  assignMember(memberID: string, teamID: string): Observable<IMessage> {
+    const header = {
+      headers: new HttpHeaders().set(
+        'Authorization',
+        `Bearer ${this.cookieService.get('userToken')}`
+      ),
+    };
+
+    return this.http.post<IMessage>(
+      environment.api_url + '/teams/assignMember',
+      { teamID: teamID, memberID: memberID },
+      header
+    );
+  }
+
+  deleteMember(memberID: string, teamID: string): Observable<IMessage> {
+    const header = {
+      headers: new HttpHeaders().set(
+        'Authorization',
+        `Bearer ${this.cookieService.get('userToken')}`
+      ),
+    };
+
+    return this.http.post<IMessage>(
+      environment.api_url + '/teams/deleteMember',
+      { teamID: teamID, memberID: memberID },
+      header
+    );
+  }
+
+  assignLeader(memberID: string, teamID: string): Observable<IMessage> {
+    const header = {
+      headers: new HttpHeaders().set(
+        'Authorization',
+        `Bearer ${this.cookieService.get('userToken')}`
+      ),
+    };
+
+    return this.http.post<IMessage>(
+      environment.api_url + '/teams/assignLeader',
+      { teamID: teamID, memberID: memberID },
+      header
+    );
+  }
+}
